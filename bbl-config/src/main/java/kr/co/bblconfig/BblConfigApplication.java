4a5a0bcd6877713a618a397987d5a80a1d8ca4cf
홍혁준 <jjj8337721@naver.com>
2023-03-22 01:33:04 +0900

    refactor

---
+++ bbl-config/src/main/java/kr/co/bblconfig/BblConfigApplication.java
@@ -0,0 +1,15 @@
+package kr.co.bblconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BblConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BblConfigApplication.class, args);
+    }
+
+}
---
+++ bbl-config/src/test/java/kr/co/bblconfig/BblConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bblconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BblConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
